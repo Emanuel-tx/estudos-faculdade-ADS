6ee1e45fb3580c171607df9bdf72a3552597e64a
Emanuel-Teixeira <emanueldtxra@gmail.com>
2025-11-09 06:43:27 -0300

    adicionar os arquivos da aula 14 de frontend

---
+++ desenvolvimento frontend/aula14 frontend/script.js
@@ -0,0 +1,27 @@
+//alert('esse js ta fora')
+
+//let nome = prompt('digite seu nome')
+//let idade = Number(prompt('digite sua idade'))
+//concatenacao de strings
+//alert(nome + ", welcome to de jangol")
+
+
+//
+//for(let i = 0; i < 3; i++){
+//    alert(i)
+//}
+
+//hile(true){
+//    numero = prompt('digite 0 para sair')
+//    if (numero == 0){
+//        break
+//    }
+//}
+const opcao = 1
+
+switch(opcao){
+    case 1:
+        alert(to)
+    case 2:
+        alert(brd)
+}
---
+++ desenvolvimento frontend/aula14 frontend/tde/script.js
@@ -0,0 +1,7 @@
+// let num1 = prompt('digite um numero')
+// let num2 = prompt('digite outro numero')
+// let resultado = Number(num1) * Number(num2)
+// alert(resultado + ' é o numero resultante da multiplicacao dos 2 numeros')
+
+let num = prompt('digite um numero')
+if(num%2 == 0)
